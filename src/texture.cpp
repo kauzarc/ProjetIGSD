b3b1c16de4057b9b2f92f6fbb8ca7c5774870493
kauzarc <jujurolland91@gmail.com>
2019-11-26 16:28:43 +0100

    texture class finished

--- src/texture.cpp
+++ src/texture.cpp
@@ -1,6 +1,5 @@
 #include "texture.h"
 
-#include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
 #include <iostream>
@@ -11,13 +10,26 @@ Texture::Texture(const string &path)
 {
     glGenTextures(1, &m_ID);
 
-    cv::Mat image = cv::imread(path, CV_LOAD_IMAGE_UNCHANGED);
+    m_image = cv::imread(path, CV_LOAD_IMAGE_UNCHANGED);
 
-    unsigned char pixel1 = *image.ptr();
-    unsigned char pixel2 = *(image.ptr() + 1);
-    unsigned char pixel3 = *(image.ptr() + 2);
+    bind();
 
-    std::cout << (unsigned int)pixel1 << std::endl;
-    std::cout << (unsigned int)pixel2 << std::endl;
-    std::cout << (unsigned int)pixel3 << std::endl;
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, m_image.cols, m_image.rows, 0, GL_BGRA, GL_UNSIGNED_BYTE, m_image.ptr());
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    unbind();
+}
+
+void Texture::bind()
+{
+    glBindTexture(GL_TEXTURE_2D, m_ID);
+}
+void Texture::unbind()
+{
+    glBindTexture(GL_TEXTURE_2D, 0);
 }
--- src/texture.h
+++ src/texture.h
@@ -5,17 +5,19 @@
 #include <vector>
 
 #include <GL/glew.h>
+#include <opencv2/core/core.hpp>
 
 class Texture
 {
 private:
     GLuint m_ID;
-    std::vector<float> m_image;
+    cv::Mat m_image;
 public:
     Texture(const std::string &path);
     ~Texture() = default;
 
     void bind();
+    void unbind();
 };
 
 #endif
