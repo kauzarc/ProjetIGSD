bf45ec37e309404ff7da9b32bb869a3655d4b5c9
kauzarc <jujurolland91@gmail.com>
2019-11-29 16:48:20 +0100

    controle

--- src/controler.cpp
+++ src/controler.cpp
@@ -20,12 +20,21 @@ void Controler::check(const Shader &shader)
 {
     shader.bind();
 
-    angleRot = (angleRot - M_PI * 0.001);
+    if (glfwGetKey(m_window, GLFW_KEY_A))
+    {
+        std::cout << "A" << angleRot << std::endl;
+        angleRot = (angleRot - M_PI * 0.001);
+    }
+    if (glfwGetKey(m_window, GLFW_KEY_D))
+    {
+        std::cout << "D" << angleRot << std::endl;
+        angleRot = (angleRot + M_PI * 0.001);
+    }
 
-    m_viewMatrix.value = glm::lookAt(
-        glm::vec3(sin(angleRot), 0, cos(angleRot)), // where is the camara
-        glm::vec3(0, 0, 0),                         //where it looks
-        glm::vec3(0, 1, 0)                          // head is up
+    m_viewMatrix.value = lookAt(
+        glm::vec3(sin(angleRot), 0, cos(angleRot)),    // where is the camara
+        glm::vec3(0, 0, 0), //where it looks
+        glm::vec3(0, 0, 1)   // head is up
     );
 
     glUniformMatrix4fv(m_viewMatrix.id, 1, GL_FALSE, glm::value_ptr(m_viewMatrix.value));
--- src/window.cpp
+++ src/window.cpp
@@ -17,14 +17,15 @@ Window::Window() : m_renderer(1024, 768),
     m_viewMatrix.id = glGetUniformLocation(m_shader.getProgramId(), "u_viewMatrix");
     m_projectionMatrix.id = glGetUniformLocation(m_shader.getProgramId(), "u_projectionMatrix");
 
-    m_projectionMatrix.value = glm::ortho(-1.0f * 768.0f / 1024.0f, 1.0f * 768.0f / 1024.0f, -1.0f * 1024.0f / 768.0f, 1.0f * 1024.0f / 768.0f, -3.0f, 3.0f);
-    // m_projectionMatrix.value = glm::mat4(1.0);
-    m_viewMatrix.value = lookAt(
-        glm::vec3(0, 0, 1),    // where is the camara
-        glm::vec3(0, 0, -0.1), //where it looks
-        glm::vec3(0, 1.0, 0)   // head is up
-    );
+    //m_projectionMatrix.value = glm::ortho(-1.0f * 768.0f / 1024.0f, 1.0f * 768.0f / 1024.0f, -1.0f * 1024.0f / 768.0f, 1.0f * 1024.0f / 768.0f, -3.0f, 3.0f);
+    // m_viewMatrix.value = lookAt(
+    //     glm::vec3(0, 0, 1),    // where is the camara
+    //     glm::vec3(0, 0, -0.1), //where it looks
+    //     glm::vec3(0, 1.0, 0)   // head is up
+    // );
 
+    m_viewMatrix.value = glm::mat4(1.f);
+    m_projectionMatrix.value = glm::mat4(1.0);
     m_modelMatrix.value = glm::mat4(1.0);
 
     m_shader.bind();
